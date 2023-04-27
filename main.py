801852089e1f9ee1ea368821850159e344cbafff
o_utkin <utkin.oleksii@gmail.com>
2023-04-27 20:35:19 +0300

    grammar

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ n = input('Please enter your number: ')
 
 # in case user has entered not a number (yes I found it in the internet :p)
 try:
-    is_number = float(n)
+    n = float(n)
 except ValueError:
     print('Please use only digits')
     exit()
 
 # checking that number is even or not
-n = float(n)
-even = n % 2 == 0
+is_even = n % 2 == 0
 
 # all the task conditions
-if not even and n > 20:
+if not is_even and n > 20:
     print('Not Weird')
-elif even and 6 <= n <= 20:
+elif is_even and 6 <= n <= 20:
     print('Weird')
-elif even and 2 <= n <= 5:
+elif is_even and 2 <= n <= 5:
     print('Not Weird')
-elif not even:
+elif not is_even:
     print('Weird')
 
 # this was not in the task condition, but I had tested this scenario too
-elif even:
+elif is_even:
     print('This wasn\'t in task condition')
